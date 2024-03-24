e3631f322b87b24247a4067c28455d8c88525e02
ol.n.nikiforova <ol.n.nikiforova@tinkoff.ru>
2024-03-24 17:36:06 +0300

    refactoring

--- app/pages/router.py
+++ app/pages/router.py
@@ -4,12 +4,16 @@ from fastapi.templating import Jinja2Templates
 
 from app.hotels.router import get_hotels_by_location_and_time
 
-router = APIRouter(prefix='/pages', tags=['Frontend'])
+router = APIRouter(prefix="/pages", tags=["Frontend"])
 
-templates = Jinja2Templates(directory='app/templates')
+templates = Jinja2Templates(directory="app/templates")
 
 
-@router.get('/hotels', response_class=HTMLResponse)
-async def get_hotels_page(request: Request,
-                          hotels=Depends(get_hotels_by_location_and_time)):
-    return templates.TemplateResponse(name='hotels.html', context={'request': request, 'hotels': hotels}, )
+@router.get("/hotels", response_class=HTMLResponse)
+async def get_hotels_page(
+    request: Request, hotels=Depends(get_hotels_by_location_and_time)
+):
+    return templates.TemplateResponse(
+        name="hotels.html",
+        context={"request": request, "hotels": hotels},
+    )
