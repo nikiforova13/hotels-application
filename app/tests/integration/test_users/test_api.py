62f74642efe7ed00fe9770be3381995ae132aa7c
ol.n.nikiforova <ol.n.nikiforova@tinkoff.ru>
2024-03-29 23:55:15 +0300

    added function

---
+++ app/tests/integration/test_users/test_api.py
@@ -0,0 +1,42 @@
+from httpx import AsyncClient
+import pytest
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("hello83@mail.ru", "8723990", 200),
+        ("hello83@mail.ru", "8723990", 409),
+        ("test", "8723990", 422),
+    ],
+)
+async def test_register_user(
+    email,
+    password,
+    status_code,
+    client: AsyncClient,
+):
+    response = await client.post(
+        "/auth/register", json={"email": email, "password": password}
+    )
+    assert response.status_code == status_code
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("test@test.com", "test", 200),
+        ("artem@example.com", "artem", 200),
+        ("nouser@example.com", "not", 401),
+    ],
+)
+async def test_login_user(
+    email,
+    password,
+    status_code,
+    client: AsyncClient,
+):
+    response = await client.post(
+        "/auth/login", json={"email": email, "password": password}
+    )
+    assert response.status_code == status_code
---
+++ app/tests/unit/test_dao.py
@@ -0,0 +1,19 @@
+from app.users.dao import UserDAO
+import pytest
+
+
+@pytest.mark.parametrize(
+    "user_id,email, user_exsist",
+    [
+        (1, "test@test.com", True),
+        (2, "artem@example.com", True),
+        (10000, ".....", False),
+    ],
+)
+async def test_find_user_by_id(user_id, email, user_exsist):
+    user = await UserDAO.find_by_id(model_id=user_id)
+    if user_exsist:
+        assert user.id == user_id
+        assert user.email == email
+    else:
+        assert not user
